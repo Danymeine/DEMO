89e303d62103697cd06fc72dca2a3a6ecdd93206
Mister_Meine <mr.daniil1999@list.ru>
2020-07-26 23:50:48 +0300

    1.0

---
+++ Demo/Panda.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Panda
+    {
+        public double _height;
+        public double _weight;
+        public string _name;
+        public Uri linkToPhoto;
+
+        public void crawl() {
+            _weight = _weight - 1;
+        }
+        public void eat() {
+            _weight = _weight + 1;
+        }
+        public string say() {
+            return "Hi";
+        }
+
+    }
+    class random
+    {
+        Random Iv = new Random();
+        public int Color = Iv.Next
+        System.ConsoleColor color = (System.ConsoleColor)Iv.Next(1, 15);
+
+    }
+
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i =1; i < 11; i++)
+            {
+                int a = i;
+                if (i > 15) { a = a - 15; };
+                Console.ForegroundColor = (System.ConsoleColor)a;
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+ 
+}
